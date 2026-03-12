87b84ee6f13c2042fc374cac5bdb04d8ea6cc68c
SNOWNUMB <mamayev.stas@gmail.com>
2026-03-12 14:44:53 +0500

    feat: Добавлен класс EventBus для реализации паттерна Pub/Sub

---
+++ Lab_09/lab_9.1/pubsub/EventBus.js
@@ -0,0 +1,66 @@
+export class EventBus {
+	constructor() {
+		this.events = new Map();
+		this.onceEvents = new Map();
+		
+	}
+
+	subscribe(event, callback) {
+		if (!this.events.has(event)) {
+			this.events.set(event, []);
+		}
+		const subscribers = this.events.get(event);
+		subscribers.push(callback);
+
+		return () => {
+			const index = subscribers.indexOf(callback);
+			if (index > -1) {
+				subscribers.splice(index, 1);
+			}
+		};
+	}
+
+	subscribeOnce(event, callback) {
+		if (!this.onceEvents.has(event)) {
+			this.onceEvents.set(event, []);
+		}
+		this.onceEvents.get(event).push(callback);
+	}
+
+	publish(event, data) {
+		if (this.onceEvents.has(event)) {
+			const onceSubscribers = this.onceEvents.get(event);
+			onceSubscribers.forEach(callback => callback(data));
+			this.onceEvents.delete(event);
+		}
+
+		if (this.events.has(event)) {
+			this.events.get(event).forEach(callback => {
+				try {
+					callback(data);
+				} catch (error) {
+					console.error(`Error occurred while publishing event '${event}':`, error);
+				}
+			});
+		}
+	}
+
+	clear(event) {
+		if (event) {
+			this.events.delete(event);
+			this.onceEvents.delete(event);
+		} else {
+			this.events.clear();
+			this.onceEvents.clear();
+		}
+	}
+
+	getSubscriberCount(event) {
+		const regular = this.events.get(event)?.length || 0;
+		const once = this.onceEvents.get(event)?.length || 0;
+		return regulare + once;
+	}
+}
+
+const eventBus = new EventBus();
+export default eventBus;
