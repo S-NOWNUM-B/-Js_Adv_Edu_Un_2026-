532a0eb6cfad68a7dc0e99163021808f953d5d9c
SNOWNUMB <mamayev.stas@gmail.com>
2026-03-19 11:34:59 +0500

    feat: Добавлен класс TodoController для управления задачами и взаимодействия с моделью и представлением

---
+++ Lab_10/lab_10.1/mvc/controller/TodoCotrooler.js
@@ -0,0 +1,18 @@
+import { TodoModel } from "../model/TodoModel.js";
+import { TodoView } from "../view/TodoView.js";
+
+export class TodoController {
+	constructor(containerId) {
+		this.model = new TodoModel();
+		this.view = new TodoView(containerId);
+
+		this.view.onAddTodo = (text) => this.model.addTodo(text);
+		this.view.onToggleTodo = (id) => this.model.toggleTodo(id);
+		this.view.onDeleteTodo = (id) => this.model.deleteTodo(id);
+		this.view.onEditTodo = (id, text) => this.model.updateTodo(id, text);
+
+		this.model.subscribe(this.view);
+
+		this.view.render(this.midel.getTodos());
+	}
+}
